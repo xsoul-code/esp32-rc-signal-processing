3029c300fff60f846b283761f874f6f3ee3ac4d5
xsoul-code <olek.zachwieja@gmail.com>
2026-03-02 00:10:42 +0100

    Add Python script for signal visualization

---
+++ plot_signal.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+raw = [float(x) for x in open('raw_values.csv')][:100]
+filtered = [float(x) for x in open('filtered_values.csv')][:100]
+
+plt.figure(figsize=(12, 6))
+plt.plot(raw, label='RAW', alpha=0.7)
+plt.plot(filtered, label='FILTERED', alpha=0.7)
+plt.title('RC Circuit Signal - RAW vs FILTERED')
+plt.xlabel('Sample')
+plt.ylabel('ADC Value')
+plt.legend()
+plt.grid(True)
+plt.savefig('signal_plot.png')
+plt.show()
